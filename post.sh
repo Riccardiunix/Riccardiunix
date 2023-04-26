1ce1f151a67740012c01360239a6fb2a4546d383
riccardiunix <riccardo.bordon.2014@gmail.com>
2023-04-26 15:00:15 +0200

    fix

--- post.sh
+++ post.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-#export CARGO_HOME=/home/orion/.local/share/cargo
-#export PATH=~/.local/bin:~/.local/share/cargo/bin:$PATH
-
-sudo pacman -S pcmanfm geckodriver pavucontrol xsecurelock scrot geany telegram-desktop dino zathura zathura-pdf-mupdf mesa libva-mesa-driver mesa-vdpau pipx python python-pip arc-gtk-theme arc-icon-theme xorg-xrdb xss-lock redshift qbittorent ccache irqbalance thermald pigz pbzip2 --needed -y &
+sudo pacman -S rustup pcmanfm sscache geckodriver cargo pavucontrol scrot geany telegram-desktop dino zathura zathura-pdf-mupdf mesa libva-mesa-driver mesa-vdpau python-pip arc-gtk-theme arc-icon-theme qbittorrent --needed
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -S librewolf-bin jellyfin-media-player-git ananicy-cpp ananicy-rules-git noisetorch ff2mpv-native-messaging-host-librewolf-git profile-sync-daemon-librewolf universal-android-debloater -y
+paru -S librewolf-bin jellyfin-media-player ananicy-cpp ananicy-rules-git noisetorch ff2mpv-native-messaging-host-librewolf-git profile-sync-daemon-librewolf universal-android-debloater
+
+systemctl enable --now ananicy-cpp
 
 #cargo install cargo-update parallel-disk-usage 
--- pre.sh
+++ pre.sh
@@ -1,17 +1,37 @@
 #!/bin/sh
-sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf &
-sudo echo "permit keepenv persist orion" > /etc/doas.conf &
+sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf || exit 1
+sudo sed -i '/BUILDDIR/s/^#//g' /etc/pacman.conf || exit 1
+echo "permit keepenv persist orion" | sudo tee /etc/doas.conf
 sudo sed -i -e 's/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q --threads=0 -)/g' /etc/makepkg.conf
 sudo sed -i -e 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z --threads=0 -)/g' /etc/makepkg.conf
+
+sudo pacman -S pigz pbzip2 ccache--needed || exit 1
+
 sudo sed -i -e 's/COMPRESSGZ=(gzip -c -f -n)/COMPRESSGZ=(pigz-c -f -n)/g' /etc/makepkg.conf
 sudo sed -i -e 's/COMPRESSBZ2=(bzip2 -c -f)/COMPRESSBZ2=(pbzip2 -c -f)/g' /etc/makepkg.conf
+sudo sed -i -e 's/!ccache/ccache/g' /etc/makepkg.conf
 
-sudo pacman -S base-devel sscache bswpm sxhkd dmenu sx wmname cargo git rxvt-unicode zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting lf htop doas neovim dash bat man tlp ccache irqbalance thermald pigz pbzip2 firefox --needed -y
+sudo pacman -S base-devel bspwm sxhkd dmenu sx wmname rxvt-unicode exa zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting lf htop doas neovim dash bat man tlp irqbalance thermald firefox xorg-xrdb xorg-xset xss-lock xsecurelock terminus-font ttf-dejavu terminator python-pipx xorg-mkfontscale --needed || exit 1
+
+pipx install trash-cli || exit 1
 
 sudo systemctl enable --now irqbalance &
 sudo systemctl enable --now thermald &
 sudo systemctl enable --now tlp &
-mkdir ~/.local/share/zsh
 
+cd /usr/share/fonts/misc
+sudo mkfontscale
+sudo mkfontdir
+
+mkdir -p ~/.local/share/zsh ~/.local/share/urxvt ~/.local/share/gnupg
 cp -r .config ~/.config
+cp -r .local ~/.local
+cp .Xdefaults ~/.config/Xdefaults
+cp .zshrc ~
 chsh -s /bin/zsh
+
+echo '''ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="bfq"
+ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none" ''' | sudo tee /etc/udev/rules.d/60-ioschedulers.rules
+
+logout
