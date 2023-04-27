b78f089e33d6a2510699c7d2106781eca42c4197
riccardiunix <riccardo.bordon.2014@gmail.com>
2023-04-27 21:35:06 +0200

    fix and improvment

--- post.sh
+++ post.sh
@@ -1,17 +1,35 @@
 #!/bin/sh
-sudo pacman -S rust pcmanfm sccache geckodriver cargo pavucontrol scrot geany telegram-desktop dino zathura zathura-pdf-mupdf libva-mesa-driver mesa-vdpau python-pip arc-gtk-theme arc-icon-theme qbittorrent libva-vdpau-driver libvdpau-va-gl gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly libde265 gst-plugin-pipewire gstreamer-vaapi adriconf --needed
+sudo pacman -S rust pcmanfm sccache geckodriver cargo pavucontrol scrot geany telegram-desktop dino zathura zathura-pdf-mupdf libva-mesa-driver mesa-vdpau python-pip arc-gtk-theme arc-icon-theme qbittorrent libva-vdpau-driver libvdpau-va-gl gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly libde265 gst-plugin-pipewire gstreamer-vaapi adriconf lazygit mesa-utils p7zip mpv yt-dlp linux-tools fzf --needed
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
-makepkg -si
+makepkg -si || exit 1
+cd ..
+
+/bin/rm -rf paru
 
 sudo sed -i '/BottomUp/s/^#//g
 /\[bin\]/s/^#//g
 /Sudo = doas/s/^#//g' /etc/paru.conf
 
-paru -S librewolf-bin jellyfin-media-player ananicy-cpp ananicy-rules-git noisetorch ff2mpv-native-messaging-host-librewolf-git profile-sync-daemon-librewolf universal-android-debloater herbe
+paru -S librewolf-bin jellyfin-media-player ananicy-cpp ananicy-rules-git noisetorch ff2mpv-native-messaging-host-librewolf-git profile-sync-daemon-librewolf herbe tlpui nsxiv --needed
+
+librewolf
 
 systemctl enable --now ananicy-cpp
 systemctl --user enable --now psd.service
 
 rm ~/.bash*
+
+git clone https://github.com/arkenfox/user.js
+
+killall librewolf
+
+LIBREWOLF_DIR=$(ls -d ~/.librewolf/*/ | fzf)
+cp user.js/user.js $LIBREWOLF_DIR
+cp user.js/prefsCleaner.sh $LIBREWOLF_DIR
+cp user.js/updater.sh $LIBREWOLF_DIR
+cp .mozilla/user-overrides.js $LIBREWOLF_DIR
+/bin/rm -rf user.js
+
+sudo powertop --calibrate
--- pre.sh
+++ pre.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf || exit 1
+echo '[device-mac-randomization]
+wifi.scan-rand-mac-address=yes
+[connection-mac-randomization]
+ethernet.cloned-mac-address=random
+wifi.cloned-mac-address=random
+[connection]
+ipv6.ip6-privacy=2' | sudo tee /etc/NetworkManager/conf.d/wifi_rand_mac.conf || exit 1
+nmcli device wifi connect "SSID" password "Password" hidden yes
+
+sudo sed -i '/ParallelDownloads/s/^#//g
+/Color/s/^#//g' /etc/pacman.conf
 printf "PKGEXT='.pkg.tar'\nPKGEXT='.pkg.tar.lz4'" | sudo tee -a /etc/makepkg.conf > /dev/null
 sudo sed -i '/BUILDDIR/s/^#//g
 /MAKEFLAGS=/s/^#//g
 s/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q --threads=0 -)/g
 s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z --threads=0 -)/g
 s/"-march=x86-64/"-march=native/g
-s/#RUSTFLAGS="-C opt-level=2"/RUSTFLAGS="-C opt-level=2 -C target-cpu=native"/g' /etc/makepkg.conf ||
-exit 1
+s/#RUSTFLAGS="-C opt-level=2"/RUSTFLAGS="-C opt-level=2 -C target-cpu=native"/g' /etc/makepkg.conf
 
 sudo pacman -S pigz pbzip2 ccache --needed || exit 1
 
@@ -15,7 +24,7 @@ sudo sed -i 's/COMPRESSGZ=(gzip -c -f -n)/COMPRESSGZ=(pigz-c -f -n)/g
 s/COMPRESSBZ2=(bzip2 -c -f)/COMPRESSBZ2=(pbzip2 -c -f)/g
 s/!ccache/ccache/g' /etc/makepkg.conf
 
-sudo pacman -S base-devel bspwm sxhkd dmenu sx wmname rxvt-unicode exa zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting lf htop doas neovim dash bat man tlp irqbalance thermald firefox xorg-xrdb xorg-xset xss-lock xsecurelock terminus-font ttf-dejavu terminator python-pipx xorg-mkfontscale --needed || exit 1
+sudo pacman -S base-devel bspwm sxhkd dmenu sx wmname rxvt-unicode exa zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting lf htop doas neovim dash bat man tlp irqbalance thermald firefox xorg-xrdb xorg-xset xss-lock xsecurelock terminus-font ttf-dejavu ttf-hack terminator python-pipx xorg-mkfontscale ethtool smartmontools tlp-rdw powertop udisks2 --needed || exit 1
 
 pipx install trash-cli || exit 1
 
@@ -23,11 +32,9 @@ sudo systemctl enable --now irqbalance
 sudo systemctl enable --now thermald
 sudo systemctl enable --now tlp
 
-mkdir -p ~/.local/share/zsh ~/.local/share/urxvt ~/.local/share/gnupg ~/.config/zsh &
+mkdir -p ~/.local/share/zsh ~/.local/share/urxvt ~/.local/share/gnupg &
 cp -r .config ~ &
 cp -r .local ~ &
-cp .Xdefaults ~/.config/Xdefaults &
-cp .zshrc ~/.config/zsh
 chsh -s /bin/zsh
 
 echo 'permit keepenv persist orion' | sudo tee /etc/doas.conf > /dev/null
@@ -36,11 +43,10 @@ echo '''ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", A
 ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="bfq"
 ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none" ''' | sudo tee /etc/udev/rules.d/60-ioschedulers.rules > /dev/null
 
-#sudo mkdir/etc/zsh
 echo "export ZDOTDIR=$HOME/.config/zsh" | sudo tee /etc/zsh/zshenv > /dev/null
 
 sudo cp 99-sysctl.conf /etc/sysctl.d/
-sudo sysctl --system
+sudo sysctl --system > /dev/null
 
 cd /usr/share/fonts/misc
 sudo mkfontscale
