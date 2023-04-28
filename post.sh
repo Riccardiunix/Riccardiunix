7dd32611d68b0bfcaa400a7455aa9cf043d401af
riccardiunix <riccardo.bordon.2014@gmail.com>
2023-04-28 23:09:15 +0200

    fix

--- post.sh
+++ post.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 /bin/rm ~/.bash* &
-sudo pacman -S rust pcmanfm sccache geckodriver cargo pavucontrol scrot geany telegram-desktop dino zathura zathura-pdf-mupdf libva-mesa-driver mesa-vdpau python-pip arc-gtk-theme arc-icon-theme qbittorrent libva-vdpau-driver libvdpau-va-gl gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly libde265 gst-plugin-pipewire gstreamer-vaapi adriconf lazygit mesa-utils p7zip mpv yt-dlp linux-tools fzf devtools rsync newsboat lxsession --needed
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
@@ -11,23 +10,21 @@ sudo sed -i '/BottomUp/s/^#//g ; /\[bin\]/s/^#//g ; /Sudo = doas/s/^#//g' /etc/p
 
 /bin/rm -rf paru &
 
-paru -S librewolf-bin jellyfin-media-player ananicy-cpp ananicy-rules-git noisetorch ff2mpv-native-messaging-host-librewolf-git profile-sync-daemon-librewolf herbe tlpui nsxiv asp --needed
+paru -S wmname pcmanfm geckodriver pavucontrol scrot geany telegram-desktop dino zathura zathura-pdf-mupdf libva-mesa-driver mesa-vdpau python-pip arc-gtk-theme arc-icon-theme qbittorrent libva-vdpau-driver libvdpau-va-gl gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly libde265 gst-plugin-pipewire gstreamer-vaapi adriconf lazygit mesa-utils p7zip mpv yt-dlp linux-tools devtools rsync newsboat lxsession ethtool tlp irqbalance thermald smartmontools tlp-rdw powertop udisks2 bc librewolf-bin jellyfin-media-player ananicy-cpp ananicy-rules-git noisetorch ff2mpv-native-messaging-host-librewolf-git profile-sync-daemon-librewolf herbe tlpui nsxiv asp github-cli --needed
 
-setsid -f librewolf > /dev/null &
-systemctl enable --now ananicy-cpp
+sudo systemctl enable --now ananicy-cpp --quiet &
+sudo systemctl enable --now irqbalance --quiet &
+sudo systemctl enable --now thermald --quiet &
+sudo systemctl enable --now tlp --quiet &
+
+librewolf -CreateProfile "main /home/orion/.librewolf/main" &
 
 git clone https://github.com/arkenfox/user.js
 
-killall -q librewolf
-
-LIBREWOLF_DIR=$(ls -d ~/.librewolf/*/ | fzf)
-
-cp user.js/user.js $LIBREWOLF_DIR &
-cp user.js/prefsCleaner.sh $LIBREWOLF_DIR &
-cp user.js/updater.sh $LIBREWOLF_DIR &
-cp .mozilla/user-overrides.js $LIBREWOLF_DIR
+cp user.js/user.js ~/.librewolf/main &
+cp user.js/prefsCleaner.sh ~/.librewolf/main &
+cp user.js/updater.sh ~/.librewolf/main &
+cp .mozilla/user-overrides.js ~/.librewolf/main
 /bin/rm -rf user.js &
 
 systemctl --user enable --now psd.service
-
-sudo powertop --calibrate
--- pre.sh
+++ pre.sh
@@ -1,28 +1,38 @@
 #!/bin/sh
-printf '[device-mac-randomization]\nwifi.scan-rand-mac-address=yes\n[connection-mac-randomization]\nethernet.cloned-mac-address=random\nwifi.cloned-mac-address=random\n[connection]\nipv6.ip6-privacy=2' | sudo tee /etc/NetworkManager/conf.d/wifi_rand_mac.conf >/dev/null || exit 1
+echo '[device-mac-randomization]
+wifi.scan-rand-mac-address=yes
+[connection-mac-randomization]
+ethernet.cloned-mac-address=random
+wifi.cloned-mac-address=random
+[connection]
+ipv6.ip6-privacy=2' | sudo tee /etc/NetworkManager/conf.d/wifi_rand_mac.conf >/dev/null || exit 1
 echo 'ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", ATTR{mtu}="1500", ATTR{tx_queue_len}="2000"' | sudo tee /etc/udev/rules.d/10-network.rules >/dev/null &
-printf "PKGEXT='.pkg.tar'\nPKGEXT='.pkg.tar.lz4'" | sudo tee -a /etc/makepkg.conf >/dev/null &
-printf 'ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"\nACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="bfq"\nACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"' | sudo tee /etc/udev/rules.d/60-ioschedulers.rules >/dev/null &
+echo "PKGEXT='.pkg.tar'
+PKGEXT='.pkg.tar.lz4'" | sudo tee -a /etc/makepkg.conf >/dev/null &
+echo 'ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="bfq"
+ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"' | sudo tee /etc/udev/rules.d/60-ioschedulers.rules >/dev/null &
+echo 'SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-1]", RUN+="/usr/bin/systemctl hibernate"
+SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="/bin/xbacklight -set 0"' | sudo tee /etc/udev/rules.d/99-lowbat.rules >/dev/null &
 sudo sed -i '/ParallelDownloads/s/^#//g ; /Color/s/^#//g' /etc/pacman.conf &
 sudo sed -i '/BUILDDIR/s/^#//g ; /MAKEFLAGS=/s/^#//g ; s/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q --threads=0 -)/g ; s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z --threads=0 -)/g ; s/"-march=x86-64/"-march=native/g ; s/#RUSTFLAGS="-C opt-level=2"/RUSTFLAGS="-C opt-level=2 -C target-cpu=native"/g' /etc/makepkg.conf &
 
 echo -n "SSID: ";read SSID
-[ -z $SSID ] || echo -n "PSW: ";read PSW;nmcli device wifi connect "$SSID" password "$PSW" hidden yes
+if [ $SSID != "" ]; then
+	echo -n "PSW: ";read PSW
+	nmcli device wifi connect "\"$SSID\"" password "\"$PSW\"" hidden yes
+fi
 
 sudo pacman -S pigz pbzip2 ccache --needed || exit 1
 
 sudo sed -i 's/COMPRESSGZ=(gzip -c -f -n)/COMPRESSGZ=(pigz-c -f -n)/g ; s/COMPRESSBZ2=(bzip2 -c -f)/COMPRESSBZ2=(pbzip2 -c -f)/g ; s/!ccache/ccache/g' /etc/makepkg.conf
 
-sudo pacman -S base-devel bspwm sxhkd dmenu sx wmname rxvt-unicode exa zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting lf htop doas neovim dash bat man tlp irqbalance thermald firefox xorg-xrdb xorg-xset xss-lock xsecurelock terminus-font ttf-dejavu ttf-hack terminator python-pipx xorg-mkfontscale ethtool smartmontools tlp-rdw powertop udisks2 --needed || exit 1
+sudo pacman -S bspwm sxhkd dmenu sx rxvt-unicode exa zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting lf htop doas neovim dash bat man-db xorg-xrdb xorg-xset xorg-mkfontscale xorg-xbacklight xss-lock xsecurelock terminus-font ttf-dejavu ttf-hack terminator python-pipx rust sccache base-devel --needed || exit 1
 
-pipx install trash-cli || exit 1
+pipx install trash-cli &
 
-echo 'permit keepenv persist orion' | sudo tee /etc/doas.conf > /dev/null &
-echo 'export ZDOTDIR=$HOME/.config/zsh' | sudo tee /etc/zsh/zshenv > /dev/null&
-
-sudo systemctl enable --now irqbalance
-sudo systemctl enable --now thermald
-sudo systemctl enable --now tlp &
+echo 'permit keepenv persist orion' | sudo tee /etc/doas.conf >/dev/null &
+echo 'export ZDOTDIR=$HOME/.config/zsh' | sudo tee /etc/zsh/zshenv >/dev/null &
 
 mkdir -p ~/.local/share/zsh ~/.local/share/urxvt ~/.local/share/gnupg &
 cp -r .config ~ &
@@ -33,8 +43,6 @@ sudo sysctl --system -q &
 
 cd /usr/share/fonts/misc
 sudo mkfontscale
-sudo mkfontdir
+sudo mkfontdir &
 
 chsh -s /bin/zsh
-
-echo "Execute a 'logout' and re-login"
