a36260b25b795f23397f03d2d1c78fd976787eba
Riccardiunix <riccardo.bordon.2014@gmail.com>
2023-12-28 16:18:58 +0100

    fix

--- pre.sh
+++ pre.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo sed -i 's/#SHA_CRYPT_MIN_ROUNDS 5000/SHA_CRYPT_MIN_ROUNDS 10000/g ; s/#SHA_CRYPT_MAX_ROUNDS 5000/SHA_CRYPT_MAX_ROUNDS 10000/g ; s/UMASK        022/UMASK       027/g' /etc/login.defs || exit 1
+sudo sed -i 's/#SHA_CRYPT_MIN_ROUNDS 5000/SHA_CRYPT_MIN_ROUNDS 10000/g ; s/#SHA_CRYPT_MAX_ROUNDS 5000/SHA_CRYPT_MAX_ROUNDS 10000/g ; s/UMASK        022/UMASK 027/g' /etc/login.defs || exit 1
 printf "* hard core 0\n* soft core 0" | sudo tee -a /etc/security/limits.conf >/dev/null
 printf "[device-mac-randomization]\nwifi.scan-rand-mac-address=yes\n[connection-mac-randomization]\nethernet.cloned-mac-address=random\nwifi.cloned-mac-address=random\n[connection]\nipv6.ip6-privacy=2" | sudo tee /etc/NetworkManager/conf.d/wifi_rand_mac.conf >/dev/null
 echo 'ACTION=="add",SUBSYSTEM=="net",KERNEL=="wl*",ATTR{mtu}="1500",ATTR{tx_queue_len}="2000"' | sudo tee /etc/udev/rules.d/10-network.rules >/dev/null
@@ -15,26 +15,28 @@ sudo curl https://big.oisd.nl/hosts > /etc/hosts
 read HNAME < /etc/hostname
 printf "\n\n127.0.0.1 localhost\n127.0.1.1 $HNAME.lan $HNAME" | sudo tee -a /etc/hosts >/dev/null
 
-sudo pacman -S pigz pbzip2 ccache mold --noconfirm --needed || exit 1
+sudo pacman -S pigz pbzip2 ccache mold doas --noconfirm --needed || exit 1
 
-sudo sed -i 's/COMPRESSGZ=(gzip -c -f -n)/COMPRESSGZ=(pigz-c -f -n)/g ; s/COMPRESSBZ2=(bzip2 -c -f)/COMPRESSBZ2=(pbzip2 -c -f)/g ; s/!ccache/ccache/g ; s/#RUSTFLAGS="-C opt-level=2"/RUSTFLAGS="-C opt-level=2 -C target-cpu=native -C link-arg=-fuse-ld=mold"/g ; s/LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"/LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -fuse-ld=mold"/g' /etc/makepkg.conf
+echo "permit keepenv persist $(whoami)" | sudo tee /etc/doas.conf >/dev/null
+sudo ln -sf /bin/doas /usr/local/bin/sudo
 
-sudo pacman -S bspwm sxhkd dmenu sx rxvt-unicode eza zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting lf htop doas neovim dash bat man-db xorg-xrdb xorg-xset xorg-mkfontscale terminus-font ttf-dejavu ttf-hack terminator pcmanfm trash-cli xorg-xrandr rust sccache base-devel xclip file-roller acpilight --noconfirm --needed || exit 1
+doas sed -i 's/COMPRESSGZ=(gzip -c -f -n)/COMPRESSGZ=(pigz-c -f -n)/g ; s/COMPRESSBZ2=(bzip2 -c -f)/COMPRESSBZ2=(pbzip2 -c -f)/g ; s/!ccache/ccache/g ; s/#RUSTFLAGS="-C opt-level=2"/RUSTFLAGS="-C opt-level=2 -C target-cpu=native -C link-arg=-fuse-ld=mold"/g ; s/LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"/LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -fuse-ld=mold"/g' /etc/makepkg.conf
 
-echo 'permit keepenv persist orion' | sudo tee /etc/doas.conf >/dev/null
-echo 'export ZDOTDIR=$HOME/.config/zsh' | sudo tee /etc/zsh/zshenv >/dev/null
+doas pacman -S bspwm sxhkd dmenu sx rxvt-unicode eza zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting lf htop neovim dash bat man-db xorg-xrdb xorg-xset xorg-mkfontscale terminus-font ttf-dejavu ttf-hack terminator pcmanfm trash-cli xorg-xrandr rust sccache base-devel xclip file-roller acpilight --noconfirm --needed || exit 1
+
+echo 'export ZDOTDIR=$HOME/.config/zsh' | doas tee /etc/zsh/zshenv >/dev/null
 
 mkdir -p ~/.local/share/zsh ~/.local/share/urxvt ~/.local/share/gnupg ~/.config/git ~/Applications ~/.local/share/android ~/.local/state/mpd ~/.ssh/sockets
 cp -r {.config, .local, .ssh} ~
-sudo cp 99-sysctl.conf /etc/sysctl.d/
+doas cp 99-sysctl.conf /etc/sysctl.d/
 
-sudo sysctl --system -q
+doas sysctl --system -q
 
 cd /usr/share/fonts/misc
-sudo mkfontscale
-sudo mkfontdir
+doas mkfontscale
+doas mkfontdir
 
-sudo usermod -aG video,wheel,uucp,adm,disk,optical $(whoami)
+doas usermod -aG video,wheel,uucp,adm,disk,optical $(whoami)
 
 chsh -s /bin/zsh
 
